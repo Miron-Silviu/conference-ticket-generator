cac1240c665431735e56f4eb3f235b77f0351736
Silviu <silviu_miron_90@outlook.com>
2025-03-26 23:43:06 +0000

    wip : Create variables for selected classes and Id

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 'use strict';
 
-function sum(a, b) {
-  return a + b / 2;
-}
+// Naming variables
 
-module.exports = sum;
+const imgInput = document.getElementById('imgInp');
+
+// Buttons
+const mainBtn = document.querySelector('.main__btn');
+const changeBtn = document.querySelector('.change');
+const removeBtn = document.querySelector('.remove');
+
+// Inputs
+
+const nameInp = document.querySelector('.input__name');
+const emailInp = document.querySelector('.email__input');
+const gitHubInp = document.querySelector('.github__input');
